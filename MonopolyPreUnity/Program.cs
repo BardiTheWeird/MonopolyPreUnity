9679ae670ef5b1bd6a9039620bcb466d86d9dfad
BardiTheWeird <48732117+BardiTheWeird@users.noreply.github.com>
2020-10-06 18:40:56 +0300

    Add project files.

---
+++ MonopolyPreUnity/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MonopolyPreUnity
+{
+    /// <summary>
+    /// Test stuff. Initialization and stuff
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
