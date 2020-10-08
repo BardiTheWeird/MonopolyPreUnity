dcc2c62d6f640d8054552bab3e4992eb8d77f65b
BardiTheWeird <48732117+BardiTheWeird@users.noreply.github.com>
2020-10-09 00:50:31 +0300

    Property abstract class instead of IProperty

---
+++ MonopolyPreUnity/Interfaces/Property.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonopolyPreUnity.Interfaces
+{
+    class Property : ITileComponent
+    {
+        string _name;
+        public string Name { get => _name; }
+        string _set; // a string for now
+        public string Set { get => _set; }
+        uint _basePrice;
+        public uint BasePrice { get => _basePrice; }
+
+        IPlayer _owner;
+        public IPlayer Owner { get; set; }
+        bool _isMortgaged;
+        public bool IsMortgaged { get; set; }
+
+        public Property(string name, string set, uint basePrice)
+        {
+            _name = name;
+            _basePrice = basePrice; 
+            _set = set;
+            _owner = null;
+            _isMortgaged = false;
+        }
+
+        public void ChargeRent(IPlayer player)
+        {
+
+        }
+
+        public void OnPlayerLanded(IPlayer player)
+        {
+            if(Owner == null)
+            {
+
+            }
+            else if (!Owner.Equals(player))
+            {
+                ChargeRent(player);
+            }
+            else
+            {
+                // do nothing
+            }
+        }
+    }
+}
