86fa6d724bb6edd0d3ed97e72111a4fee0b45e4c
BardiTheWeird <48732117+BardiTheWeird@users.noreply.github.com>
2020-10-09 21:54:00 +0300

    Revert "Revert "Create request system bardi""

---
+++ MonopolyPreUnity/Classes/Request.cs
@@ -0,0 +1,21 @@
+﻿using MonopolyPreUnity.Managers;
+using MonopolyPreUnity.Utitlity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonopolyPreUnity.Classes
+{
+    class Request<TChoices, TExpectedInput>
+    {
+        public RequestType RequestType { get; }
+        public TChoices Choices { get; }
+        public TExpectedInput Input { get; set; }
+
+        public Request(int receiverId, RequestType requestType, TChoices choices)
+        {
+            RequestType = requestType;
+            Choices = choices;
+        }
+    }
+}
