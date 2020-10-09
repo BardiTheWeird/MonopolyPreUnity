fb01250522aba9206545b02ea4bbe821cd7e26d9
BardiTheWeird <48732117+BardiTheWeird@users.noreply.github.com>
2020-10-09 22:40:11 +0300

    Added Player constructor

--- MonopolyPreUnity/Classes/Player.cs
+++ MonopolyPreUnity/Classes/Player.cs
@@ -9,9 +9,19 @@ namespace MonopolyPreUnity.Classes
     {
         public int Id { get; }
         public string DisplayName { get; }
-        public List<Property> Properties { get; set; }
+        public List<Property> Properties { get; }
         public int Cash { get; set; }
         public int JailCards { get; set; }
-        public int TurnsInPrison { get; set; }
+        public int? TurnsInPrison { get; set; }
+
+        public Player(int id, string displayName, int cash)
+        {
+            Id = id;
+            DisplayName = displayName;
+            Properties = new List<Property>();
+            Cash = cash;
+            JailCards = 0;
+            TurnsInPrison = null;
+        }
     }
 }
