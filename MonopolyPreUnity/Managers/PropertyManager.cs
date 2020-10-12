f680f989e9910efbb49d4ca789dce9e77774ad22
hell0ut <vlad.poplavsckyi@gmail.com>
2020-10-12 16:20:15 +0300

    Added methods and not implemented exceptions.

--- MonopolyPreUnity/Managers/PropertyManager.cs
+++ MonopolyPreUnity/Managers/PropertyManager.cs
@@ -6,5 +6,40 @@ namespace MonopolyPreUnity.Managers
 {
     class PropertyManager
     {
-    }
+
+
+        public void GetAvailableActions()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public void BuildHouse()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void SellHouse()
+        {
+
+
+            throw new NotImplementedException();
+
+        }
+        public void Mortage()
+        {
+
+            throw new NotImplementedException();
+
+        }
+        
+        public void UnMortage()
+        {
+
+
+            throw new NotImplementedException();
+        }
+
+
+        }
 }
