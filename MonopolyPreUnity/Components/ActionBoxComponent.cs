9af7d919afbabc8e6bed838d326c7c2e921a5532
BardiTheWeird <bardianofmandalore@gmail.com>
2020-10-14 12:39:04 +0300

    expanded components a bit. Still in work

---
+++ MonopolyPreUnity/Components/ActionBoxComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonopolyPreUnity.Components
+{
+    class ActionBoxComponent : ITileContentComponent
+    {
+        public void OnPlayerLanded(int playerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MonopolyPreUnity/Components/ActionComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonopolyPreUnity.Components
+{
+    class ActionComponent : ITileContentComponent
+    {
+        public void OnPlayerLanded(int playerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
