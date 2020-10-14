5ed86b350dfe1bf3eb4a7cc1fdeeab6d62298bf2
BardiTheWeird <bardianofmandalore@gmail.com>
2020-10-14 15:10:43 +0300

    finished basic content component stuff

---
+++ MonopolyPreUnity/Components/FreeParkingComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonopolyPreUnity.Components
+{
+    class FreeParkingComponent : ITileContentComponent
+    {
+        public void OnPlayerLanded(int playerId)
+        {
+            // Kinda do nothing
+        }
+    }
+}
