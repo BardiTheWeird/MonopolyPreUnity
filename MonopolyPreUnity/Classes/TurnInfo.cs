5481dc1cfaf5b75e7958cb542a3ac2cb6860c874
BardiTheWeird <bardianofmandalore@gmail.com>
2020-10-15 22:50:59 +0300

    added restrictions to generic types in TileManager

---
+++ MonopolyPreUnity/Classes/TurnInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonopolyPreUnity.Classes
+{
+    class TurnInfo
+    {
+        public List<int> turnOrder { get; }
+        public int curTurnPlayer { get; }
+    }
+}
