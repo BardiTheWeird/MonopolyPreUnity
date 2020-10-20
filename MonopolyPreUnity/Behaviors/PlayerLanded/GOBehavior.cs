fcf1b57bbc4ae174c44725bb11d9c2bac7fe3b8f
kokokojo2 <60895503+kokokojo2@users.noreply.github.com>
2020-10-20 19:01:32 +0300

    some behaviors adeed

---
+++ MonopolyPreUnity/Behaviors/PlayerLanded/GOBehavior.cs
@@ -0,0 +1,25 @@
+﻿using MonopolyPreUnity.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using MonopolyPreUnity.Managers;
+using MonopolyPreUnity.Classes;
+
+namespace MonopolyPreUnity.Behaviors.PlayerLanded
+{
+    class GOBehavior : IPlayerLandedBehavior
+    {
+        #region Dependencies
+        private readonly PlayerManager _playerManager;
+        #endregion
+
+        private int amount;
+
+        public void PlayerLanded(int playerId, ITileComponent tileComponent, int tileId)
+        {
+            _playerManager.PlayerCashGive(playerId, amount);
+            Logger.Log($"Player recieved {amount} for passing the GO.");
+        }
+    }
+}
