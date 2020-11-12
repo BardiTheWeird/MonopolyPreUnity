9fe32072c9cd0f7d5257539d35f30a7a88b8ee79
BardiTheWeird <bardianofmandalore@gmail.com>
2020-11-12 02:53:34 +0200

    added auto-scrollable output text-box

---
+++ GameInterface/CustomControls/ScrollingTextBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace GameInterface.CustomControls
+{
+    class ScrollingTextBox : TextBox
+    {
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+            VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+            HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+        }
+
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            base.OnTextChanged(e);
+            CaretIndex = Text.Length;
+            ScrollToEnd();
+        }
+    }
+}
