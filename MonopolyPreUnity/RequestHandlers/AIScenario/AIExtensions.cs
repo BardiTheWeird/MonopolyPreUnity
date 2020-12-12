f6377faedb63c314add8f3d2f0b18da6f7a18693
BardiTheWeird <bardianofmandalore@gmail.com>
2020-12-12 02:32:20 +0200

    added weighted randomness

--- MonopolyPreUnity/RequestHandlers/AIScenario/AIExtensions.cs
+++ MonopolyPreUnity/RequestHandlers/AIScenario/AIExtensions.cs
@@ -1,7 +1,10 @@
 ﻿using MonopolyPreUnity.Utitlity;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using Weighted_Randomizer;
 
 namespace MonopolyPreUnity.RequestHandlers.AIScenario
 {
@@ -9,7 +12,20 @@ namespace MonopolyPreUnity.RequestHandlers.AIScenario
     {
         public static MonopolyCommand ChaosChoice(this List<(MonopolyCommand, int)> commandsWeight, ChaosFactor factor) 
         {
-            throw new NotImplementedException();
+            if (factor == 0)
+            {
+                var maxWeight = commandsWeight.Max(x => x.Item2);
+                return commandsWeight.First(x => x.Item2 == maxWeight).Item1;
+            }
+
+            var mean = commandsWeight.Sum(x => x.Item2) / (double)commandsWeight.Count;
+            var ratio = Math.Max(factor / (double)10, 0.95f);
+            commandsWeight.ForEach(x => x.Item2 += (int)((mean - x.Item2) * ratio));
+
+            var weightedRand = new StaticWeightedRandomizer<MonopolyCommand>(Guid.NewGuid().GetHashCode());
+            commandsWeight.ForEach(x => weightedRand.Add(x.Item1, x.Item2));
+
+            return weightedRand.NextWithReplacement();
         }
     }
 }
