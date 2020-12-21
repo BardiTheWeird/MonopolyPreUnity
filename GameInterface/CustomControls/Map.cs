a0671b11df90b56bcfea31b0a91d0358913f515d
BardiTheWeird <bardianofmandalore@gmail.com>
2020-12-21 23:59:01 +0200

    created the base for stuff, beginnings of custom controls and renders

---
+++ GameInterface/CustomControls/Map.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace GameInterface.CustomControls
+{
+    //[ContentProperty("Content")]
+    class Map : FrameworkElement
+    {
+        public static readonly DependencyProperty MapImagePathProperty =
+            DependencyProperty.Register(
+            "MapImagePath", typeof(string),
+            typeof(Map)
+            );
+        public string MapImagePath
+        {
+            get => (string)GetValue(MapImagePathProperty); 
+            set => SetValue(MapImagePathProperty, value); 
+        }
+
+        private ImageSource mapImageSource;
+
+        public Map()
+        {
+            mapImageSource = new BitmapImage(new Uri(@"D:\ImmaCodder\Kursach\MonopolyPreUnity\GameInterface\Images\map.png"));
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            Debug.WriteLine($"RenderWidth:{RenderSize.Width}; RenderHeight:{RenderSize.Height}");
+            drawingContext.DrawImage(mapImageSource, new Rect(RenderSize));
+        }
+    }
+}
---
+++ GameInterface/CustomControls/MapPanel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace GameInterface.CustomControls
+{
+    class MapPanel : Panel
+    {
+        public MapPanel()
+            : base()
+        {
+
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            return availableSize;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            foreach (UIElement child in InternalChildren)
+            {
+                child.Arrange(new Rect(new Point(0, 0), finalSize));
+            }
+            return finalSize; // Returns the final Arranged size
+        }
+    }
+}
