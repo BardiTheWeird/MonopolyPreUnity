4613b65181f2b2407b4f9c992123f80f2995f215
BardiTheWeird <bardianofmandalore@gmail.com>
2020-12-23 00:58:00 +0200

    created math for getting rectangles of the tiles by index

--- GameInterface/CustomControls/Map.cs
+++ GameInterface/CustomControls/Map.cs
@@ -75,9 +75,14 @@ namespace GameInterface.CustomControls
         #endregion
 
         #region field
+        int botRightIndex = 0;
+        int botLeftIndex = 10;
+        int topLeftIndex = 20;
+        int topRightIndex = 30;
+
         double imageSide = 6800;
-        Size tile = new Size(832, 575);
-        Size bigTile = new Size(832, 832);
+        Size tile = new Size(832, 577.5);
+        Size bigTile = new Size(815, 815);
 
         Size actualTileSize => new Size(RenderSize.Height * tile.Height / imageSide,
             RenderSize.Height * tile.Width / imageSide);
@@ -87,6 +92,7 @@ namespace GameInterface.CustomControls
         double tileWidth => ActualWidth - 2 * actualBigTileSize.Width;
         #endregion
 
+        #region ctor
         public Map()
         {
             mapImageSource = new BitmapImage(new Uri(@"D:\ImmaCodder\Kursach\MonopolyPreUnity\GameInterface\Images\map.png"));
@@ -94,13 +100,34 @@ namespace GameInterface.CustomControls
             ClickedTileId = -1;
             MouseLeftButtonUp += Map_MouseLeftButtonUp;
         }
+        #endregion
+
+        #region drawing groups
+        DrawingGroup rectangleGroup = new DrawingGroup();
+        #endregion
 
         #region click mathematics
         private void Map_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             PointClicked = e.GetPosition((UIElement)sender);
             ClickedTileId = GetClickedTileInfo(PointClicked);
+            DrawClickedTileRect();
             //Debug.WriteLine($"Well duh, I tried, {PointClicked}");
+        }
+
+        void DrawClickedTileRect()
+        {
+            if (rectangleGroup.Children.Count > 0)
+                rectangleGroup.Children.RemoveAt(0);
+
+            var rect = GetTileRectangle(ClickedTileId);
+            var geomDrawing = new GeometryDrawing(
+                new SolidColorBrush(Color.FromRgb(0, 0, 0)),
+                new Pen(Brushes.Black, 4),
+                new RectangleGeometry(rect));
+
+            rectangleGroup.Children.Add(geomDrawing);
+            InvalidateVisual();
         }
 
         enum Position
@@ -112,10 +139,6 @@ namespace GameInterface.CustomControls
         {
             Position vert;
             Position hor;
-            int botRight = 0;
-            int botLeft = 10;
-            int topLeft = 20;
-            int topRight = 30;
 
             // vertical position
             if (point.Y <= actualTileSize.Height)
@@ -139,12 +162,12 @@ namespace GameInterface.CustomControls
                 {
                     // count the tile
                     var offset = (int)((point.X - actualBigTileSize.Width) / actualTileSize.Width);
-                    return topLeft + offset + 1;
+                    return topLeftIndex + offset + 1;
                 }
                 else if (hor == Position.left)
-                    return topLeft;
+                    return topLeftIndex;
                 else
-                    return topRight;
+                    return topRightIndex;
             }
             else if (vert == Position.bottom)
             {
@@ -152,12 +175,12 @@ namespace GameInterface.CustomControls
                 {
                     // count the tile
                     var offset = 8 - (int)((point.X - actualBigTileSize.Width) / actualTileSize.Width);
-                    return botRight + offset + 1;
+                    return botRightIndex + offset + 1;
                 }
                 else if (hor == Position.left)
-                    return botLeft;
+                    return botLeftIndex;
                 else
-                    return botRight;
+                    return botRightIndex;
             }
             else
             {
@@ -165,17 +188,77 @@ namespace GameInterface.CustomControls
                 {
                     // count the tile
                     var offset = 8 - (int)((point.Y - actualBigTileSize.Height) / actualTileSize.Width);
-                    return botLeft + offset + 1;
+                    return botLeftIndex + offset + 1;
                 }
                 else if (hor == Position.right)
                 {
                     // count the tile
                     var offset = (int)((point.Y - actualBigTileSize.Height) / actualTileSize.Width);
-                    return topRight + offset + 1;
+                    return topRightIndex + offset + 1;
                 }
                 else
                     return -1;
             }
+        }
+        #endregion
+
+        #region mapRenderingMath
+        Rect GetTileRectangle(int index)
+        {
+            Point origin;
+            Size size;
+            bool invert = false;
+
+            if (index <= botLeftIndex) // bottom row
+            {
+                origin = new Point(0, ActualHeight - actualTileSize.Height);
+
+                if (index != botLeftIndex)
+                {
+                    if (index == botRightIndex)
+                        origin.X = ActualWidth - actualBigTileSize.Width;
+                    else
+                    {
+                        origin.X += actualBigTileSize.Width;
+                        origin.X += actualTileSize.Width * (9 - index % 10);
+                    }
+                }
+            }
+            else if (index < topLeftIndex) // left column
+            {
+                invert = true;
+                origin = new Point(0, actualBigTileSize.Height);
+                origin.Y += actualTileSize.Width * (9 - index % 10);
+            }
+            else if (index <= topRightIndex) // top row
+            {
+                origin = new Point(0, 0);
+
+                if (index != topLeftIndex)
+                {
+                    if (index == topRightIndex)
+                        origin.X = ActualWidth - actualBigTileSize.Width;
+                    else
+                    {
+                        origin.X += actualBigTileSize.Width;
+                        origin.X += actualTileSize.Width * (index % 10 - 1);
+                    }
+                }
+            }
+            else // right column
+            {
+                invert = true;
+                origin = new Point(ActualWidth - actualBigTileSize.Width, actualBigTileSize.Height);
+                origin.Y += actualTileSize.Width * (index % 10 - 1);
+            }
+
+            if (index % 10 == 0)
+                size = actualBigTileSize;
+            else if (invert)
+                size = new Size(actualTileSize.Height, actualTileSize.Width);
+            else
+                size = actualTileSize;
+            return new Rect(origin, size);
         }
         #endregion
 
@@ -184,6 +267,7 @@ namespace GameInterface.CustomControls
             base.OnRender(drawingContext);
             //Debug.WriteLine($"RenderWidth:{RenderSize.Width}; RenderHeight:{RenderSize.Height}");
             drawingContext.DrawImage(mapImageSource, new Rect(RenderSize));
+            drawingContext.DrawDrawing(rectangleGroup);
         }
     }
 }
